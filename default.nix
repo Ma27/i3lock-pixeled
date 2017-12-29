391b804d69fc032208a2a9cf7f6a18bf15eca475
Maximilian Bosch <maximilian@mbosch.me>
2017-12-29 23:19:05 +0100

    Cleanup dev environment

--- default.nix
+++ default.nix
@@ -1,12 +1,8 @@
 with import <nixpkgs>{};
 
-stdenv.mkDerivation rec {
-  name = "i3lock-pixeled-${version}";
-  version = "1.1.0";
-
+stdenv.mkDerivation {
+  name = "i3lock-pixeled-dev";
   src = ./.;
 
-  buildInputs = with pkgs; [ i3lock imagemagick scrot playerctl ];
-  buildPhases = [ "unpackPhase" "installPhase" ];
-  makeFlags = [ "PREFIX=$(out)/bin" ];
+  buildInputs = [ i3lock imagemagick scrot playerctl ];
 }
